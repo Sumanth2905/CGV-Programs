f5c014f4fce3d6b4270c2aeff33c5765e424d936
Sumanth K <48558948+Sumanth2905@users.noreply.github.com>
2021-05-16 23:31:06 +0530

    Code to draw lines using GL_LINES
    C++ code to draw lines by giving the points.

---
+++ Lines/Line.cpp
@@ -0,0 +1,31 @@
+// Code to draw lines using GL_LINES
+#include<GL/glut.h>
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex2f(380.0, 260.0);
+	glVertex2f(250.0, 230.0);
+	glVertex2f(370.0, 230.0);
+	glVertex2f(220.0, 200.0);
+	glEnd();
+	glFlush();
+}
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+
+}
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(5, 5);
+	glutCreateWindow("Lines using glut");
+	glutDisplayFunc(display);
+	init();
+	glutMainLoop();
+}
